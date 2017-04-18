5cf4842434ed3b070d9fde6ca40b18b5c626b29a
anhtuanpro1996 <ksdsk1372@gmail.com>
2017-04-18 09:25:06 +0700

    Nguyễn Anh Tuấn
    Pull

---
+++ QL_KHO/QL_Kho/DT0/ChiTietXuat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Kho.DT0
+{
+    class ChiTietXuat
+    {
+        private string maCTX;
+        private string maPX;
+        private string maHH;
+        private string soLuong;
+        private string donGia;
+
+        public string MaCTX
+        {
+            get
+            {
+                return maCTX;
+            }
+
+            set
+            {
+                maCTX = value;
+            }
+        }
+        public string MaPX
+        {
+            get
+            {
+                return maPX;
+            }
+
+            set
+            {
+                maPX = value;
+            }
+        }
+        public string MaHH
+        {
+            get
+            {
+                return maHH;
+            }
+
+            set
+            {
+                maHH = value;
+            }
+        }
+        public string SoLuong
+        {
+            get
+            {
+                return soLuong;
+            }
+
+            set
+            {
+                soLuong = value;
+            }
+        }
+        public string DonGia
+        {
+            get
+            {
+                return donGia;
+            }
+
+            set
+            {
+                donGia = value;
+            }
+        }
+    }
+}
---
+++ QL_KHO/QL_Kho/DT0/PhieuXuat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Kho.DT0
+{
+    class PhieuXuat
+    {
+        private string maPX;
+        private string ngayXuat;
+        private string tongTien;
+        public string MaPX
+        {
+            get
+            {
+                return maPX;
+            }
+
+            set
+            {
+                maPX = value;
+            }
+        }
+        public string NgayXuat
+        {
+            get
+            {
+                return ngayXuat;
+            }
+
+            set
+            {
+                ngayXuat = value;
+            }
+        }
+        public string TongTien
+        {
+            get
+            {
+                return tongTien;
+            }
+
+            set
+            {
+                tongTien = value;
+            }
+        }
+    }
+}
