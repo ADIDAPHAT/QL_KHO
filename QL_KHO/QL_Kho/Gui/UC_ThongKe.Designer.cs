6ffef6177fc5fb5ef4214761e1ffc51c6de7321f
ADIDAPHAT <adidaphat0605@gmail.com>
2017-04-25 08:08:24 +0700

    HT 25/4
    thong ke

--- QL_KHO/QL_Kho/Gui/UC_ThongKe.Designer.cs
+++ QL_KHO/QL_Kho/Gui/UC_ThongKe.Designer.cs
@@ -31,18 +31,22 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvtkhh = new System.Windows.Forms.DataGridView();
+            this.rdb_hh = new System.Windows.Forms.RadioButton();
+            this.rdb_nhap = new System.Windows.Forms.RadioButton();
+            this.rdb_xuat = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvtkhh)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(604, 105);
+            this.panel1.Size = new System.Drawing.Size(687, 105);
             this.panel1.TabIndex = 18;
             // 
             // label1
@@ -60,30 +64,74 @@
             this.dgvtkhh.AllowUserToAddRows = false;
             this.dgvtkhh.AllowUserToDeleteRows = false;
             this.dgvtkhh.AllowUserToResizeRows = false;
+            this.dgvtkhh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvtkhh.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvtkhh.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dgvtkhh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvtkhh.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvtkhh.Location = new System.Drawing.Point(0, 110);
+            this.dgvtkhh.Location = new System.Drawing.Point(0, 176);
             this.dgvtkhh.Margin = new System.Windows.Forms.Padding(2);
             this.dgvtkhh.Name = "dgvtkhh";
             this.dgvtkhh.RowTemplate.Height = 24;
             this.dgvtkhh.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvtkhh.Size = new System.Drawing.Size(604, 346);
+            this.dgvtkhh.Size = new System.Drawing.Size(687, 222);
             this.dgvtkhh.TabIndex = 46;
+            // 
+            // rdb_hh
+            // 
+            this.rdb_hh.AutoSize = true;
+            this.rdb_hh.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_hh.Location = new System.Drawing.Point(94, 132);
+            this.rdb_hh.Name = "rdb_hh";
+            this.rdb_hh.Size = new System.Drawing.Size(88, 21);
+            this.rdb_hh.TabIndex = 47;
+            this.rdb_hh.TabStop = true;
+            this.rdb_hh.Text = "Hàng hóa";
+            this.rdb_hh.UseVisualStyleBackColor = true;
+            this.rdb_hh.CheckedChanged += new System.EventHandler(this.rdb_hh_CheckedChanged);
+            // 
+            // rdb_nhap
+            // 
+            this.rdb_nhap.AutoSize = true;
+            this.rdb_nhap.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_nhap.Location = new System.Drawing.Point(251, 132);
+            this.rdb_nhap.Name = "rdb_nhap";
+            this.rdb_nhap.Size = new System.Drawing.Size(123, 21);
+            this.rdb_nhap.TabIndex = 48;
+            this.rdb_nhap.TabStop = true;
+            this.rdb_nhap.Text = "Hàng hóa nhập";
+            this.rdb_nhap.UseVisualStyleBackColor = true;
+            this.rdb_nhap.CheckedChanged += new System.EventHandler(this.rdb_nhap_CheckedChanged);
+            // 
+            // rdb_xuat
+            // 
+            this.rdb_xuat.AutoSize = true;
+            this.rdb_xuat.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_xuat.Location = new System.Drawing.Point(421, 132);
+            this.rdb_xuat.Name = "rdb_xuat";
+            this.rdb_xuat.Size = new System.Drawing.Size(120, 21);
+            this.rdb_xuat.TabIndex = 49;
+            this.rdb_xuat.TabStop = true;
+            this.rdb_xuat.Text = "Hàng hóa xuất";
+            this.rdb_xuat.UseVisualStyleBackColor = true;
+            this.rdb_xuat.CheckedChanged += new System.EventHandler(this.rdb_xuat_CheckedChanged);
             // 
             // UC_ThongKe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.rdb_xuat);
+            this.Controls.Add(this.rdb_nhap);
+            this.Controls.Add(this.rdb_hh);
             this.Controls.Add(this.dgvtkhh);
             this.Controls.Add(this.panel1);
             this.Name = "UC_ThongKe";
-            this.Size = new System.Drawing.Size(604, 456);
+            this.Size = new System.Drawing.Size(690, 401);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvtkhh)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,5 +140,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvtkhh;
+        private System.Windows.Forms.RadioButton rdb_hh;
+        private System.Windows.Forms.RadioButton rdb_nhap;
+        private System.Windows.Forms.RadioButton rdb_xuat;
     }
 }
